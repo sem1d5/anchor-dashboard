2af4eed327128bba1c3e93aa6e097e96dab2320f
sem1d5 <96908030+sem1d5@users.noreply.github.com>
2022-01-01 22:11:02 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd 
 import plotly.express as px
 import numpy as np 
+import plotly.figure_factory as ff
 
 header = st.container()
 total_users = st.container()
@@ -14,25 +15,36 @@ with header:
 
 with total_users:
 	st.header('Number of users on Anchor')
-	st.text('example text')
+	#st.text('example text')
 
-	#mau_data = pd.read_csv('data/mau_anchor.csv')
+	#this shows the 3 column metrics 
+	col1, col2, col3 = st.columns(3)
+	col1.metric("Total Anchor Users", "135k", "1.2 °F")
+	col2.metric("Total Borrowers", "40k")
+	col3.metric("Total Depositors", "119k")
 
 
-	#st.table(mau_data)
 
-	#mau_data = pd.DataFrame({'mau_anchor':columns=['months', 'monthly_active_users']})
-	#mau_data['mau_anchor'] = mau_data['mau_anchor'].astype(str)
-	
-	#csv_file = 'mau_anchor.csv',sheetnames = 'DATA'
-
-	df = pd.read_excel('data/mau_anchor.xlsx', sheet_name = 'data',usecols = 'A:B', header = 0)
+	#this part reads and shows the table 
+	df = pd.read_excel('mau_anchor.xlsx', sheet_name = 'data',usecols = 'A:B', 
+						header = 0)
 
 	st.dataframe(df)
 
+	#creates the bar graph 
 	mau_bar = px.bar(df, title = 'test title', x = 'MONTHS', y = 'MONTHLY_ACTIVE_USERS')
 
 	st.plotly_chart(mau_bar)
+
+	#inserting DAU data 
+	df = pd.read_excel('dau_anchor.xlsx', sheet_name = 'Sheet 1', usecols = 'A:B',
+						header = 0)
+	st.dataframe(df)
+
+	#creates line graph for DAU 
+	dau_fig = px.line(df, title = 'dau title',x = 'DATES', y = 'DAILY_ACTIVE_USERS')
+	dau_fig.show()
+
 
 
 with daily_activity:
